b96a0e33cfd78d0b486c54bf90d0206b5fc88e3f
Gerhard Pegel <gpegel@gmail.com>
2015-09-03 23:10:40 +0200

    My first C# console application about the HelperMethod

---
+++ HelperMethod/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelperMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myValue = superSecretFormula("world");
+            Console.WriteLine(myValue);
+            Console.ReadLine();
+
+        }
+
+        private static string superSecretFormula()
+        {
+            //some cool stuf here
+            return " Hello World";
+        }
+
+        private static string superSecretFormula(string name)
+        {
+            return String.Format("Hello, {0}", name);
+        }
+    }
+}
